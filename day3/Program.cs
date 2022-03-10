2378b0e1cc38b041ebfe7d961275ba71b39103dc
MrAwsm99 <rajaksujitkumar99@gmail.com>
2022-03-10 22:27:02 +0530

    tasks

---
+++ day3/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day3
+{
+    //Task 1
+    public interface userdata
+    {
+        void data();
+    }
+    public interface premium
+    {
+        void premiumamount();
+    }
+    public class displaypremium : premium
+    {
+        public void premiumamount()
+        {
+            persondata obj = new persondata();
+
+            if (obj.age < 20)
+            {
+                Console.WriteLine("your premium amount is 10000");
+
+            }
+            else if (obj.age >= 20 && obj.age <= 30)
+            {
+                Console.WriteLine("your premium amount is 20000");
+            }
+            else
+            {
+                Console.WriteLine("your premium amount is 30000");
+            }
+        }
+    }
+    public class persondata : userdata
+    {
+        public int age;
+        public string name;
+        public dynamic dob;
+
+        //Task 2
+
+        public void data()
+        {
+            Console.WriteLine("enter your age:");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter your dob:");
+            dob = Console.ReadLine();
+            Console.WriteLine("enter your name:");
+            name = Console.ReadLine();
+
+            int[] numbers = { 55, 40, 26, 78 };
+            Console.WriteLine(numbers);
+            foreach (int n in numbers)
+                Console.WriteLine(n);
+            numbers.Max();
+            numbers.Min();
+            numbers.Sum();
+            numbers.Average();
+            Array.Sort(numbers);
+            Array.Reverse(numbers);
+            Array.BinarySearch(numbers, 10);
+            Array.ForEach(numbers, x => Console.WriteLine(x));
+
+
+            //Task 3
+            String[] male = { "Omega", "Dom", "Bruce", "Nemo" };
+            String[] female = { "Scarlet", "Pooja", "Aditi" };
+
+            Console.WriteLine("Enter your Gender:");
+            string gen = Console.ReadLine();
+
+            if (gen == "male")
+            {
+                foreach (String s in male)
+                    Console.WriteLine(s);
+            }
+            else
+            {
+                foreach (string s in female)
+                    Console.WriteLine(s);
+            }
+
+            string[] name1 = { "James", "Mint", "Dom", "john" };
+            int[] age1 = { 22, 25, 30, 26 };
+            Array.Sort(age1);
+
+            foreach (string name in name1)
+            {
+                Console.WriteLine(name);
+
+                foreach (int age in age1)
+                {
+                    Console.WriteLine(age);
+                }
+            }
+            //Task 4
+
+                ArrayList al = new ArrayList();
+
+                al.Add(10);
+                al.Add(20);
+                al.Add(true);
+
+                int[] ar = { 10, 20, 30 };
+                al.AddRange(ar);
+                Console.WriteLine(al.Count);
+
+                Queue queue = new Queue(al);
+                queue.Enqueue(10);
+                Console.WriteLine(queue.Count);
+
+                al.AddRange(queue);
+                Console.WriteLine(al.Count);
+
+            }
+        }
+    }
+
+    
---
+++ day4/Program.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Task 1
+            var customers = new List<Customer>()
+            {
+                new Customer() { Id = 1, Fname = "John", Lname = "Doe" },
+                new Customer() { Id = 2, Fname = "Mint", Lname = "Leaf" },
+                new Customer() { Id = 3, Fname = "Elo", Lname = "Omi" }
+            };
+
+            Console.WriteLine("Enter User ID : ");
+            int Id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Edit last name : ");
+            string lastname = Console.ReadLine();
+
+
+            for (int i = 0; i < customers.Count; i++)
+            {
+                if (customers[i].Id == Id)
+                {
+                    customers[i].Lname = lastname;
+                }
+            }
+
+            for (int i = 0; i < customers.Count; i++)
+            {
+                Console.WriteLine(customers[i].Fname + " " + customers[i].Lname);
+            }
+        }
+        public class Customer
+        {
+            public int Id { get; set; }
+            public string Fname { get; set; }
+            public string Lname { get; set; }
+        }
+        //Task 
+
+        String user, pass, place, pet, school;
+        int mob;
+        Console.WriteLine("Enter the username");
+            user = Console.ReadLine();
+            Console.WriteLine("Enter the password");
+            pass = Console.ReadLine();
+            Console.WriteLine("Enter the mobile number");
+            mob = int.Parse(Console.ReadLine());
+
+        Console.WriteLine();
+            if (pass == "admin" && user == "admin@mail.com" && mob == 12345)
+            { Console.WriteLine("Welcome"); }
+            else
+            {
+                Console.WriteLine("Try again");
+            }
+Console.WriteLine("forgot the password type 1");
+y = int.Parse(Console.ReadLine());
+if (y == 0)
+{
+    Console.WriteLine("see you again");
+}
+else
+{
+    Console.WriteLine("Select security question");
+    Console.WriteLine("Enter the name of your born place");
+    place = Console.ReadLine();
+    if (place == "true")
+    {
+        Console.WriteLine("Welcome");
+    }
+    else
+    {
+        Console.WriteLine("Try another question");
+        Console.WriteLine("Enter the name of pet");
+        pet = Console.ReadLine();
+        if (pet == "tom")
+        {
+            Console.WriteLine("Welcome");
+        }
+        else
+        {
+            Console.WriteLine("Try another question");
+            Console.WriteLine("Enter the name of your school");
+            school = Console.ReadLine();
+            if (place == "Hogwart")
+            {
+                Console.WriteLine("Welcome");
+            }
+            else
+            {
+                Console.WriteLine("Please Create a new Account");
+            }
+
+            //Task 
+
+            List<string> ports = new List<string>()
+            { "USA", "UK", "IND", "MAL", "AFC", "JAP"};
+            List<string> from = new List<string>();
+            List<string> to = new List<string>();
+            List<string> type = new List<string>();
+            List<int> Id = new List<int>();
+            dynamic f, t, op, id = 0, k = "y", a = 0, b = 0;
+
+            IDictionary<int, string> portid = new Dictionary<int, string>();
+
+            for (int i = 0, j = 21; i < ports.Count; i++, j++)
+            {
+                portid.Add(j, ports[i]);
+
+            }
+            while (k == "y")
+            {
+                Console.WriteLine("enter export or import");
+                op = Console.ReadLine();
+                if (op == "export")
+                {
+                    type.Add("EXPORT");
+                    Console.WriteLine("list of ports:");
+
+                    foreach (var i in ports)
+                    {
+                        Console.WriteLine(i);
+                    }
+                    Console.WriteLine("enter from adderess:");
+                    f = Console.ReadLine();
+                    Console.WriteLine("list of destination ports ports:");
+                    foreach (var i in ports)
+                    {
+                        if (f != i)
+                        {
+                            Console.WriteLine(i);
+                        }
+                    }
+                    Console.WriteLine("enter destination port:");
+                    t = Console.ReadLine();
+                    id++;
+                    from.Add(f);
+                    to.Add(t);
+                    Id.Add(id);
+                }
+                else
+                {
+                    type.Add("IMPORT");
+                    Console.WriteLine("list of ports:");
+                    foreach (var i in ports)
+                    {
+                        Console.WriteLine(i);
+                    }
+                    Console.WriteLine("enter from adderess:");
+                    f = Console.ReadLine();
+                    Console.WriteLine("list of destination ports ports:");
+                    foreach (var i in ports)
+                    {
+                        if (f != i)
+                        {
+                            Console.WriteLine(i);
+                        }
+                    }
+                    Console.WriteLine("enter destination port:");
+                    t = Console.ReadLine();
+                    id++;
+                    from.Add(f);
+                    to.Add(t);
+                    Id.Add(id);
+                }
+                Console.WriteLine("you want to  import or export again:(y/n)");
+                k = Console.ReadLine();
+                if (k == "n")
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("list of orders:");
+            for (int i = 0; i < from.Count; i++)
+            {
+                foreach (var j in portid)
+                {
+                    if (from[i] == j.Value)
+                    {
+                        a = j.Key;
+                        break;
+                    }
+                }
+                foreach (var j in portid)
+                {
+                    if (to[i] == j.Value)
+                    {
+                        b = j.Key;
+                        break;
+                    }
+                }
+                Console.WriteLine($"from port id:{a} to port id:{b} user id:{ Id[i]} type:{ type[i]}");
+            }
+        }
